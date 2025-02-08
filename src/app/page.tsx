30fcc5356d6a83105c6aadb4ce08d8dd313d58e8
Muzaffar Ali <muzafark834@gmail.com>
2025-02-09 00:32:01 +0500

    responive added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 
 import Hero from "@/components/Herro";
 import FontShowcase from "@/components/Retangle";
-import NewArivel from "../components/arrivals";
 import Dress from "@/components/dress";
 import CustomerCarousel from "@/components/Customer";
 import Products from "./product/page";
